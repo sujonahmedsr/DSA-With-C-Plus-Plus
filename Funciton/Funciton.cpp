c6e46296be1eb3bb9502c06ecfa50d7033af5f4a
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2026-01-09 16:45:07 +0600

    practice funciton

--- Funciton/Funciton.cpp
+++ Funciton/Funciton.cpp
@@ -11,10 +11,38 @@ int sumOfM(int n){
     return sum;
 }
 
+// Calculate N factorial
+int factorialOFN(int n){
+    int factorial = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+// sum of digits 
+int sumOfDigits(int num){
+    int digSum = 0;
+    
+    while (num > 0){
+        int lastDigit = num % 10;
+        num = num / 10;
+        
+        digSum += lastDigit;
+    }
+    
+    return digSum;
+}
+
 int main(){
     int n;
     cout << "Your your number: ";
     cin >> n;
-    cout << "Sum of your number is: " << sumOfM(n) << endl;
+    // cout << "Sum of your number is : " << sumOfM(n) << endl;
+    // cout << "And the factorial of " << n << " : " << factorialOFN(n) << endl;
+    
+    cout << "The sum of digit is: " << sumOfDigits(n) << endl;
+    
     return 0;
 }
